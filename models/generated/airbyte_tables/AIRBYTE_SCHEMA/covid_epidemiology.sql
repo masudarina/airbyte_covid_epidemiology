c90106b159a122a9acdcbf9cfbc0483e43f31d86
masudarina <104750749+masudarina@users.noreply.github.com>
2022-06-14 16:19:59 +0900

    Create covid_epidemiology.sql

---
+++ models/generated/airbyte_tables/AIRBYTE_SCHEMA/covid_epidemiology.sql
@@ -0,0 +1,29 @@
+{{ config(
+   schema="covid_data",
+   tags=["top-level"],
+   partition_by={
+       "field": "date",
+       "data_type": "date"
+   },
+   cluster_by=["key"]
+) }}
+-- Final base SQL model
+-- depends_on: {{ ref('0610_csvdatacovid_epidemiology_ab3') }}
+
+select
+    KEY,
+    DATE,
+    NEW_TESTED,
+    NEW_DECEASED,
+    TOTAL_TESTED,
+    NEW_CONFIRMED,
+    NEW_RECOVERED,
+    TOTAL_DECEASED,
+    TOTAL_CONFIRMED,
+    TOTAL_RECOVERED,
+    _AIRBYTE_AB_ID,
+    _AIRBYTE_EMITTED_AT,
+    {{ current_timestamp() }} as _AIRBYTE_NORMALIZED_AT,
+    _AIRBYTE_0610_CSVDATACOVID_EPIDEMIOLOGY_HASHID
+from {{ ref('0610_csvdatacovid_epidemiology_ab3') }}
+-- 0610_csvdatacovid_epidemiology from {{ source('AIRBYTE_SCHEMA', '_AIRBYTE_RAW_0610_CSVDATACOVID_EPIDEMIOLOGY') }}
